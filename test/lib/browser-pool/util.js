14c61a01da19fb3d493a7a86583357536287707d
Dmitriy Dudkevich <dudkevich@yandex-team.ru>
2021-03-08 23:31:55 +0300

    fix: ability to work correctly with wdio@7 inside hermione (#91)

--- test/lib/browser-pool/util.js
+++ test/lib/browser-pool/util.js
@@ -2,12 +2,13 @@
 
 const Promise = require('bluebird');
 
-exports.stubBrowser = function(id, version) {
+exports.stubBrowser = function(id, version, publicAPI = {}) {
     return {
         id: id || 'default-id',
         sessionId: process.hrtime().join('_'), // must be unique
         reset: sinon.stub().returns(Promise.resolve()),
         toPrint: sinon.stub().returns(id),
+        publicAPI,
         version
     };
 };
