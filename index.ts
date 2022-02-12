ca9377369d51c59b7a29600bdcd169dfcbf591a0
oldskytree <oldskytree@yandex-team.ru>
2022-02-12 20:06:53 +0300

    chore(index): rewrite index.js to TS

--- index.ts
+++ index.ts
@@ -1,27 +1,26 @@
-'use strict';
+export { default as BaseStats } from "./lib/base-stats";
+export { default as SetsBuilder } from "./lib/sets-builder";
 
-module.exports = {
-    BaseStats: require('./lib/base-stats'),
-    SetsBuilder: require('./lib/sets-builder'),
-    config: {
-        options: require('./lib/config/options')
-    },
-    Calibrator: require('./lib/calibrator'),
-    BrowserPool: require('./lib/browser-pool'),
-    BrowserAgent: require('./lib/browser-agent'),
-    Image: require('./lib/image'),
-    temp: require('./lib/temp'),
-    errors: require('./lib/errors'),
-    events: require('./lib/events'),
-    promiseUtils: require('./lib/promise-utils'),
-    clientBridge: require('./lib/client-bridge'),
-    coverage: {
-        coverageLevel: require('./lib/coverage/coverage-level')
-    },
-    browser: {
-        Camera: require('./lib/browser/camera')
-    },
-    ScreenShooter: require('./lib/screen-shooter'),
-    Viewport: require('./lib/screen-shooter/viewport'),
-    CoordValidator: require('./lib/screen-shooter/viewport/coord-validator')
-};
+import options from "./lib/config/options";
+export const config = { options };
+
+export { default as Calibrator } from "./lib/calibrator";
+export * as BrowserPool from "./lib/browser-pool";
+export { default as BrowserAgent } from "./lib/browser-agent";
+export { default as Image } from "./lib/image";
+export * as temp from "./lib/temp";
+export * as errors from "./lib/errors";
+export * as events from "./lib/events";
+export * as promiseUtils from "./lib/promise-utils";
+export * as clientBridge from "./lib/client-bridge";
+
+import * as coverageLevel from "./lib/coverage/coverage-level";
+export const coverage = { coverageLevel };
+
+import Camera from "./lib/browser/camera";
+export const browser = { Camera };
+export { Camera };
+
+export { default as ScreenShooter } from "./lib/screen-shooter";
+export { default as Viewport } from "./lib/screen-shooter/viewport";
+export { default as CoordValidator } from "./lib/screen-shooter/viewport/coord-validator";
