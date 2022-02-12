1e978926adc644fd0960e4c5366e5b73d6c6fd2c
oldskytree <oldskytree@yandex-team.ru>
2022-02-12 22:06:49 +0300

    chore(utils): rewrite module utils on TS

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,5 @@
-'use strict';
-
-exports.requireWithNoCache = function(moduleName) {
+export function requireWithNoCache(moduleName: string): NodeRequire {
     delete require.cache[moduleName];
+
     return require(moduleName);
 };
